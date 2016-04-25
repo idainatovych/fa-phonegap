317ae4aae4f47378b5ddc8daa55f6f5e23ee6332
Taras Omelianenko <t.omelianenko@apibest.com>
2016-04-25 12:47:58 +0300

    Book trainer

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,28 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('scheduleTrainerCtrl', function($scope) {
+.controller('scheduleTrainerCtrl', function($scope, $ionicPopup) {
 
+// Triggered on a button click, or some other target
+    $scope.showPopup = function() {
+        $scope.data = {};
+
+        // A confirm dialog
+        $scope.showConfirm = function() {
+            var confirmPopup = $ionicPopup.confirm({
+                title: 'Confirm appointment',
+                template: '<p class="text-center">Create appointment with <strong>Artem Davidchenko</strong> on <strong>Monday, 25 Apr 2016 from 10:00 till 11:00</strong>?</p>'
+            });
+
+            confirmPopup.then(function(res) {
+                if(res) {
+                    console.log('You are sure');
+                } else {
+                    console.log('You are not sure');
+                }
+            });
+        }
+    }();
 })
    
 .controller('accountCtrl', function($scope) {
