390d585bf20689333f155bdc287b77434a10ea9d
Dmitriy Velychko <d.velychko@disclab.com>
2016-05-12 16:33:01 +0300

    Added notification with actions to app

--- app/app.main.js
+++ app/app.main.js
@@ -1,16 +1,54 @@
 'use strict';
 
-function AppMain($ionicPlatform,  $translate) {
-    $ionicPlatform.ready(function() {
-        if(typeof navigator.globalization !== "undefined") {
-            navigator.globalization.getPreferredLanguage(function(language) {
-                $translate.use((language.value).split("-")[0]).then(function(data) {
+function AppMain($ionicPlatform, $translate) {
+    $ionicPlatform.ready(function () {
+        if (typeof navigator.globalization !== "undefined") {
+            navigator.globalization.getPreferredLanguage(function (language) {
+                $translate.use((language.value).split("-")[0]).then(function (data) {
                     console.log("SUCCESS -> " + data);
-                }, function(error) {
+                }, function (error) {
                     console.log("ERROR -> " + error);
                 });
             }, null);
         }
+
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "XXXXXXXX"
+            },
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true,
+                "categories": {
+                    "confirm": {
+                        "yes": {
+                            "callback": "app.accept", "title": "Yes", "foreground": true, "destructive": false
+                        },
+                        "no": {
+                            "callback": "app.reject", "title": "No", "foreground": true, "destructive": false
+                        }
+                    }
+                }
+            },
+            "windows": {}
+        });
+
+        push.on('error', function (e) {
+            console.log("push error = " + e.message);
+        });
+
+        push.on('registration', function (data) {
+            console.log('registration event: ' + data.registrationId);
+
+            var oldRegId = localStorage.getItem('registrationId');
+            if (oldRegId !== data.registrationId) {
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+                // Post registrationId to your app server as the value has changed
+            }
+        });
+
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
